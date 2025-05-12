4a15c2104b3284e59194edefa86e8cb4d31ad2a7
Soyeon Kim <sykim096@gmail.com>
2025-05-12 18:05:47 +1000

    Remove unnecessary code

--- university_system/model/subject.py
+++ university_system/model/subject.py
@@ -1,22 +1,11 @@
 import random
-from university_system.utils.utils import *
 
 class Subject:
     def __init__(self):
+        from university_system.university import University
         self.id = f"{random.randint(1, 999):03d}"  
         self.mark = random.randint(25, 100)        
-        self.grade = self.calcGrade(self.mark)     
-    @staticmethod
-    def calcGrade(mark):
-        if mark >= 85:
-            return "HD"
-        elif mark >= 75:
-            return "D"
-        elif mark >= 65:
-            return "C"
-        elif mark >= 50:
-            return "P"
-        return "Z"
+        self.grade = University.calcGrade(self.mark)     
 
     def get_subject_json(self):
         return {
--- university_system/university.py
+++ university_system/university.py
@@ -1,5 +1,4 @@
 import re
-
 from university_system.controllers.admin_controller import AdminController
 from university_system.controllers.student_controller import StudentController
 
@@ -23,19 +22,16 @@ class University:
                 case _:
                     print("Error: please either input A, S, or X")
 
-
     def is_valid_password(self, password):
         first_upper = password[0].isupper()
         over_5_letters = sum(c.isalpha() for c in password) >= 5
         ends_with_3num = re.search(r"\d{3,}$", password)
         return bool(first_upper and over_5_letters and ends_with_3num)
 
-    @staticmethod
-    def is_valid_email(email):
+    def is_valid_email(self, email):
         pattern = r'^[a-zA-Z0-9._%+-]+@university\.com$'
         return bool(re.match(pattern, email))
     
-    @staticmethod
     def calcGrade(mark):
         if mark >= 85:
             return "HD"
