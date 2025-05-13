0c8c9897b0d8ea88fbac73dec32dab0e7ad221c0
WhenTheRainCame <97927519+WhenTheRainCame@users.noreply.github.com>
2025-05-13 14:56:56 +1000

    Changes made to admin controller lists - added sub menu for sort

--- university_system/controllers/admin_controller_DEPRECIATED.py
+++ university_system/controllers/admin_controller_DEPRECIATED.py
@@ -15,6 +15,9 @@ class AdminController(UserController):
             print(f"{indent}(p) Partition Students (Pass/Fail)")
             print(f"{indent}(r) Remove Student")
             print(f"{indent}(s) Show Student List")
+            print(f"{indent}(si) Sort Student by ID")
+            print(f"{indent}(sn) Sort Student by Name")
+            print(f"{indent}(se) Sort Student by Email")
             print(f"{indent}(x) Exit Admin Menu")
             
             choice = input(f"{indent}Enter choice: ").lower().strip()
@@ -31,28 +34,17 @@ class AdminController(UserController):
                 case "r":
                     self.admin.remove_student()
                 case "s":
-                    while True: 
-                        print(f"{indent}(s) Show full Student List")
-                        print(f"{indent}(si) Sort Student by ID")
-                        print(f"{indent}(sn) Sort Student by Name")
-                        print(f"{indent}(se) Sort Student by Email")
-                        print(f"{indent}(x) Exit Admin Menu")  
-                        choice2 = input("How would you like to view?") 
-                        match choice2:
-                            case "s":
-                                print(f"{indent}Student List")
-                                self.admin.viewAllStudents()
-                            case "si":
-                                print(f"{indent}Student List base on ID")
-                                self.admin.sortStudent(choice)
-                            case "sn":
-                                print(f"{indent}Student List base on name")
-                                self.admin.sortStudent(choice)
-                            case "se":
-                                print(f"{indent}Student List base on Email")
-                                self.admin.sortStudent(choice)
-                            case "x":
-                                break
+                    print(f"{indent}Student List")
+                    self.admin.viewAllStudents()
+                case "si":
+                    print(f"{indent}Student List base on ID")
+                    self.admin.sortStudent(choice)
+                case "sn":
+                    print(f"{indent}Student List base on name")
+                    self.admin.sortStudent(choice)
+                case "se":
+                    print(f"{indent}Student List base on Email")
+                    self.admin.sortStudent(choice)
                 case "x":
                     break
                 case _:
